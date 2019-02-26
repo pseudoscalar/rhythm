555d28f494d1e97ac766db5e95793f3c6d393a8b
pseudoscalar <47653615+pseudoscalar@users.noreply.github.com>
2019-02-26 00:37:56 -0600

    Read timing data out of the audio system.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ use sdl2::{
 use std::{
     fs::File,
     io::{BufReader, Read, Seek},
+    sync::atomic::{AtomicUsize, Ordering},
+    sync::Arc,
     time::{Duration, Instant},
 };
 
@@ -25,7 +27,7 @@ where
     R: Read + Seek
 {
     inner: Decoder<R>,
-    samples_read: u32,
+    samples_read: Arc<AtomicUsize>,
     time_base: Instant,
 }
 
@@ -36,7 +38,7 @@ where
     fn new(inner: Decoder<R>) -> TrackingSource<R> {
         TrackingSource {
             inner: inner,
-            samples_read: 0,
+            samples_read: Arc::new(AtomicUsize::new(0)),
             time_base: Instant::now(),
         }
     }
@@ -49,11 +51,7 @@ where
     type Item = i16;
 
     fn next(&mut self) -> Option<i16> {
-        self.samples_read += 1;
-        if self.samples_read % self.inner.sample_rate() == 0 {
-            let millis = (self.samples_read as u64 * 1000) / (self.inner.sample_rate() as u64 * self.inner.channels() as u64);
-            println!("{:?} {:?}", millis, self.time_base.elapsed());
-        }
+        self.samples_read.fetch_add(1, Ordering::Relaxed);
         self.inner.next()
     }
 }
@@ -81,12 +79,18 @@ where
 
 fn main() {
     let device  = rodio::default_output_device().expect("Couldn't get default audio device");
+    let format = device.default_output_format().expect("Couldn't get default output format");
+    let samples_per_sec = format.channels as u32 * format.sample_rate.0;
     let mut sink = Sink::new(&device);
+    let loop_start = Instant::now();
 
 
-    let file = File::open("top.ogg").expect("Couldn't open file");
+    let file = File::open("top-fixed.ogg").expect("Couldn't open file");
     let source = TrackingSource::new(Decoder::new(BufReader::new(file)).expect("Couldn't decode file"));
     let silence_source = Zero::<i16>::new(source.channels(), source.sample_rate()).take_duration(Duration::new(0, 10));
+
+    let samples_read = source.samples_read.clone();
+    let source_samples_per_sec = source.channels() as u32 * source.sample_rate();
 
     sink.set_volume(0.05);
     sink.append(silence_source);
@@ -110,7 +114,6 @@ fn main() {
 
     let mut event_pump = sdl.event_pump().unwrap();
 
-    let loop_start = Instant::now();
     let mut frame_count_start = loop_start;
     let mut frame_count = 0;
     let one_second = Duration::new(1, 0);
@@ -122,7 +125,15 @@ fn main() {
                     break 'main;
                 },
                 Event::KeyDown { keycode: Some(keycode), .. } => {
-                    println!("{:?}: {:?}", loop_start.elapsed(), keycode);
+                    let samples = sink.samples_written.load(Ordering::Relaxed);
+                    let sample_time = samples as f64 / samples_per_sec as f64;
+
+                    let source_samples = samples_read.load(Ordering::Relaxed);
+                    let source_sample_time = source_samples as f64 / source_samples_per_sec as f64;
+
+                    let dur = loop_start.elapsed();
+                    let clock_time = dur.as_secs() as f64 + (dur.subsec_millis() as f64 / 1000.0);
+                    println!("{:?} {:?} {:?}", sample_time, sample_time - source_sample_time, keycode);
                 },
                 _ => {}
             }
